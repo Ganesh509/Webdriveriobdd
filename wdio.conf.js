225ea9f0b7871af9cbcd814d4ee62f94bcf3f4e7
Ganesh <ganeshpradhan1996@gmail.com>
2026-03-18 18:14:50 +0530

    updated

--- wdio.conf.js
+++ wdio.conf.js
@@ -123,7 +123,13 @@ exports.config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter
-    reporters: ['spec'],
+    reporters: [
+        ['allure', {
+            outputDir: 'allure-results',
+            disableWebdriverStepsReporting: false,
+            disableWebdriverScreenshotsReporting: false
+        }]
+    ],
 
     // If you are using Cucumber you need to specify the location of your step definitions.
     cucumberOpts: {
